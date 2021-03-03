61c478188905d99bf602c602252454536e530c57
OWRIG <a14973032@126.com>
2021-03-03 19:54:15 +0800

    v2.0

--- test.js
+++ test.js
@@ -1,3 +1,18 @@
+用到的函数有
+- text() //需要点击的文本，因为会返回所有符合数据，所以一般配合find()或findOnce使用
+- findeOnce() //从0开始，选择text()返回的第x+1个数据
+- bounds() //框选一个范围，，四个参数按左上右下的顺序代表某一个边到屏幕该边的像素距离 建议使用Autojs工具配合使用，或者手机厂商自带工具应该也有用
+- click() //模拟点击该对象
+- sleep() // zzZZ （ms）
+- input() //输入 input([i, ]text) i {number} 表示要输入的为第i + 1个输入框 text {string} 要输入的文本
+- gesture() //模拟手势操作 gesture(duration, [x1, y1], [x2, y2], ...) 模拟手势操作。
+            //例如gesture(1000, [0, 0], [500, 500], [500, 1000])为模拟一个从(0, 0)到(500, 500)到(500, 100)的手势操作，时长为2秒。
+            //这里我用来实现选项的上滑下滑，原本也有个scroll函数的，但是可能是易班设置的组件属性为不可滑动，导致无法使用（也有可能是我哪里做错了，为了快速开发，我就没有去深入了解）
+
+
+
+
+
 var btn1 = text("请选择").findOnce(0);
 var btn2 = text("请选择").findOnce(1);
 var btn3 = text("请选择").findOnce(2);
@@ -10,7 +25,6 @@ var btn9_1 = text("请选择").findOnce(7);
 var btn9_2 = text("请选择").findOnce(8);
 var btn10 = text("请选择").findOnce(9);
 
-var scrollClick = bounds(0, 1818, 1080, 1920);
 
 btn1.click();
 sleep(random(200, 300));
